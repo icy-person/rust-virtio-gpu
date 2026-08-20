4fdd84592ab7fa3f54af86d19be167f377704f01
icy-person <kar.fars.1388@gmail.com>
2026-08-20 11:12:29 +0330

    fix(transport): export PCI config-space module from correct path

--- src/virtio_gpu/transport/mod.rs
+++ src/virtio_gpu/transport/mod.rs
@@ -1,8 +1,8 @@
 pub mod memory;
 pub mod pci;
+pub mod pci_config_space;
 pub mod virtqueue;
 
 pub use memory::{GuestAddress, GuestMemory, GuestMemoryError};
-
-pub use pci::config_space::PciConfigSpace;
+pub use pci_config_space::PciConfigSpace;
 pub use virtqueue::{Descriptor, SplitVirtQueue, UsedElement, VirtQueueError};
