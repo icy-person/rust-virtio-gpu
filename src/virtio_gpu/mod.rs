03c50a5b8bbadcb3a9d3857e676a6ce0086311c2
icy-person <kar.fars.1388@gmail.com>
2026-08-20 12:18:43 +0330

    fix: include standard device extension handlers

--- src/virtio_gpu/mod.rs
+++ src/virtio_gpu/mod.rs
@@ -1,6 +1,11 @@
 pub mod device {
     include!(concat!(env!("OUT_DIR"), "/device.rs"));
 }
+
+mod device_ext;
+
+pub use device::VirtioGpuDevice;
+
 pub mod display;
 pub mod features;
 pub mod protocol;
