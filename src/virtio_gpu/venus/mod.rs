4956f606ed747de46ceb782eb42c655d839ea488
icy-person <kar.fars.1388@gmail.com>
2026-08-20 09:03:16 +0330

    feat(venus): export wire dispatcher

--- src/virtio_gpu/venus/mod.rs
+++ src/virtio_gpu/venus/mod.rs
@@ -1,8 +1,10 @@
 pub mod command;
 pub mod context;
+pub mod dispatcher;
 pub mod resource;
 pub mod state;
 
+pub use dispatcher::{VenusDispatchError, VenusResponse};
 pub use state::{
     BlobMemory, FencePoint, FenceTracker, VenusContext, VenusResource, VenusState,
     VenusStateError, VENUS_MAX_VERSION,
