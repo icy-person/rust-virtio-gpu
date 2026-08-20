ab893ae02fec56a7e7f2e81ca5b6fea6dd13dcaa
icy-person <kar.fars.1388@gmail.com>
2026-08-20 10:05:10 +0330

    fix(vulkan): make resource manager API functional

--- src/virtio_gpu/renderer/vulkan/resource_manager.rs
+++ src/virtio_gpu/renderer/vulkan/resource_manager.rs
@@ -6,19 +6,52 @@ use super::{DeviceMemory, Image};
 
 pub struct GpuTexture {
     pub image: Image,
-
     pub memory: DeviceMemory,
 }
 
+#[derive(Default)]
 pub struct ResourceManager {
-    #[allow(dead_code)]
     textures: HashMap<ResourceId, GpuTexture>,
 }
 
 impl ResourceManager {
     pub fn new() -> Self {
-        Self {
-            textures: HashMap::new(),
-        }
+        Self::default()
+    }
+
+    pub fn insert(&mut self, id: ResourceId, texture: GpuTexture) -> Option<GpuTexture> {
+        self.textures.insert(id, texture)
+    }
+
+    pub fn get(&self, id: ResourceId) -> Option<&GpuTexture> {
+        self.textures.get(&id)
+    }
+
+    pub fn get_mut(&mut self, id: ResourceId) -> Option<&mut GpuTexture> {
+        self.textures.get_mut(&id)
+    }
+
+    pub fn remove(&mut self, id: ResourceId) -> Option<GpuTexture> {
+        self.textures.remove(&id)
+    }
+
+    pub fn len(&self) -> usize {
+        self.textures.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.textures.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn manager_starts_empty() {
+        let manager = ResourceManager::new();
+        assert!(manager.is_empty());
+        assert_eq!(manager.len(), 0);
     }
 }
