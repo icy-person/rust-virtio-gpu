768f04b67bdffb271177758f0a5bb1575d7a1b34
icy-person <kar.fars.1388@gmail.com>
2026-08-20 11:15:44 +0330

    feat(transport): export PCI config builder

--- src/virtio_gpu/transport/mod.rs
+++ src/virtio_gpu/transport/mod.rs
@@ -1,8 +1,10 @@
 pub mod memory;
 pub mod pci;
+pub mod pci_config_builder;
 pub mod pci_config_space;
 pub mod virtqueue;
 
 pub use memory::{GuestAddress, GuestMemory, GuestMemoryError};
-pub use pci_config_space::PciConfigSpace;
+pub use pci_config_builder::build_pci_config_space;
+pub use pci_config_space::{PciConfigSpace, VirtioPciCfgCapability};
 pub use virtqueue::{Descriptor, SplitVirtQueue, UsedElement, VirtQueueError};
